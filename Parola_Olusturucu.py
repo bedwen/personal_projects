80ade5d708b8b9892a156b1ab1d9dd5136edc5cc
bedwen <mailforcode00@gmail.com>
2025-10-25 19:36:38 +0300

    guncelleme

--- Parola_Olusturucu.py
+++ Parola_Olusturucu.py
@@ -1,4 +1,6 @@
 import random
+
+
 print("=Şifre Oluşturucusu="
       "\n1-Büyük Harf"
       "\n2-Küçük Harf"
@@ -7,42 +9,33 @@ print("=Şifre Oluşturucusu="
       "\n5-Oluştur"
       "\nŞifrenizde olmasını istediğiniz seçenekleri numaraları ile seçiniz.")
 
-uzunluk = int(input("Şifre uzunluğunu giriniz: "))
-
+uzunluk = int(input("Şifre uzunluğunu giriniz: ")) #Kullanıcıdan input almak.
 rakam=list("0123456789")
 buyuk_h=list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 kucuk_h=list("abcdefghijklmnopqrstuvwxyz")
 ozel_k=list("!@#$%^&*")
 sifre_listesi=[]
 
-while(1):
+while(1): #Kullanıcıdan aldığımız veriyi işleyerek değerlere atamak.
     secim = int(input("Listeden seçim yapınız: "))
     if(secim==1):
         sifre_listesi.extend(buyuk_h)
-
     elif(secim==2):
         sifre_listesi.extend(kucuk_h)
-
     elif(secim==3):
         sifre_listesi.extend(rakam)
-
     elif(secim==4):
         sifre_listesi.extend(ozel_k)
-
     elif(secim==5):
         break
     else:
-        print("Lütfen geçerli seçeneklerde seçim yapınız.")
+        print("Lütfen geçerli seçeneklerde seçim yapınız.") #Kullanıcının inputlarını denetlemek ve feedback vermek.
 
 sifre=[]
-
-for i in range(uzunluk):
+for i in range(uzunluk): #Atadığımız değerler ile şifre oluşturuyoruz.
     random_karakterler = random.choice(sifre_listesi)
-
     sifre.append(random_karakterler)
-
-
-print("Şifreniz: ","".join(sifre))
+print("Şifreniz: ","".join(sifre)) #Oluşturduğumuz şifreyi output ediyoruz.
 
 
 
---
+++ def_fonksiyonu.py
@@ -0,0 +1,12 @@
+
+a = int(input("A sayısını giriniz: "))
+b = int(input("B sayısını giriniz: "))
+
+def carpma_yap(a,b):
+    carpim = a*b
+    return carpim
+
+print(carpma_yap(a,b))
+
+
+
