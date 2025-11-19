871c89f0a0736dc062e604379067269efbc5ddcc
bedwen <mailforcode00@gmail.com>
2025-11-19 22:59:18 +0300

    bro is fixed

--- bigonotation_5.py
+++ bigonotation_5.py
@@ -1,10 +1,10 @@
-def bubble_sirala(dizi):
+def kabarcik_sirala(dizi):
     n = len(dizi)
     for i in range(n):
         for j in range(n - i - 1):
             if dizi[j] > dizi[j + 1]:
                 dizi[j], dizi[j + 1] = dizi[j + 1], dizi[j]
 
-sayilar = [5, 3, 1, 7, 9, 11, 13, 15, 17, 19]
-bubble_sirala(sayilar)
+sayilar = [5, 3, 1, 7, 9, 11, 13, 15, 17, 19, 6]
+kabarcik_sirala(sayilar)
 print(sayilar)
