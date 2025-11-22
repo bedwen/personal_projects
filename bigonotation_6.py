c67822d852293a2f0fc292293b0788e4dee9d836
bedwen <mailforcode00@gmail.com>
2025-11-22 19:58:13 +0300

    fix

--- bigonotation_6.py
+++ bigonotation_6.py
@@ -1,9 +1,9 @@
-#fibonacci dizi 0 ve 1 ile başlar her seferinde önceki elemanla toplanır
-fibo = [0,1]
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
 
-for i in range(2,20): #10'u 20 yaparsak ilk 22'i elemanı bulur
-    fibo.append(fibo[i-2]+fibo[i-1])
-
-print(fibo)
+print(fibonacci(10))
 
 
